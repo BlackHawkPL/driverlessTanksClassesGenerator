b84772d2d8ec631176f219c513b530c636fa8bb1
BlackHawkPL <arturo2028@gmail.com>
2017-12-21 00:16:20 +0100

    workaround for game requiring vehicle to be local in order to drive it

--- driverlessTanksClassesGenerator/Program.cs
+++ driverlessTanksClassesGenerator/Program.cs
@@ -42,16 +42,42 @@ namespace driverlessTanksClassesGenerator
         requiredVersion = 0.1;
         requiredAddons[] = { ""A3_Armor_F"" };
         version = 1;
-        };
     };
+};
 
-    class cfgVehicles
-    {");
+class Extended_PreInit_EventHandlers
+{
+	class noDrivers
+	{
+	    init=""RHS_ENGINE_STARTUP_OFF = true;removeDrivers = {{_d = getDir _x;_p = getPosASL _x;_vd = vectorDir _x;_vu = vectorUp _x; _t = (typeOf _x) + '_dl';if (isClass (configfile >> 'CfgVehicles' >> _class)) then {deleteVehicle _x;_n = _t createVehicle [0,0,0];_n setDir _d;_n setVectorDirAndUp [_vd, _vu];_n setPosASL _p;};} forEach _this;};"";
+
+    };
+};
+class Extended_InitPost_EventHandlers
+{
+	class Tank
+	{
+	    init=""if (typeOf (_this select 0) find '_dl' != -1) then {[_this select 0, 1, ['ACE_SelfActions'], ['enable_driving','Enable driving (use if vehicle does not move)','',{[_target, clientOwner] remoteExec ['setOwner', 2]},{vehicle _player == _target && ((assignedVehicleRole _player) select 0) == 'Turret'}] call ace_interact_menu_fnc_createAction] call ace_interact_menu_fnc_addActionToObject;};"";
+
+    };
+};
+
+class Extended_getIn_EventHandlers
+{
+	class Tank
+	{
+	    getin=""if (typeOf (_this select 0) find '_dl' != -1 && {(_this select 2) == effectiveCommander (_this select 0)}) then {[_this select 0, clientOwner] remoteExec ['setOwner', 2]};"";
+
+    };
+};
+
+class cfgVehicles
+{");
 
             foreach (var className in classes)
             {
                 var classNameTrimmed = className.Trim('\"');
-                if (classNameTrimmed.Contains("_dl") || classNameTrimmed.Contains("base"))
+                if (classNameTrimmed.Contains("_dl") || classNameTrimmed.Contains("base") || classNameTrimmed.Contains("tb_"))
                     continue;
 
                 sb.AppendFormat(@"
@@ -66,9 +92,9 @@ namespace driverlessTanksClassesGenerator
 
             var result = sb.ToString();
 
-            System.IO.File.WriteAllText(@"C:\test\noDrivers\config.cpp", result);
+            System.IO.File.WriteAllText(@"C:\test\bh_noDrivers\config.cpp", result);
 
-            var strCmdText = @"/C makePbo -x none C:\test\noDrivers C:\test\";
+            var strCmdText = @"/C makePbo -x none C:\test\bh_noDrivers C:\test\";
             System.Diagnostics.Process.Start("CMD.exe", strCmdText);
         }
     }
